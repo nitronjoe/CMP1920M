bd9c3f6e28f482861c503990d80290f1ab3eedc1
nitronjoe <nitronjoe@gmail.com>
2024-04-28 21:50:48 +0100

    Add project files.

---
+++ CMP1920M/Die.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1920M
+{
+    internal class Die
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CMP1920M/Testing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1920M
+{
+    internal class Testing
+    {
+    }
+}
